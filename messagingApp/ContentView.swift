2290aa37e25bffb4bbe2e159cda1a1becb5eabd8
RockBiscuit <jackson.stoppel@gmail.com>
2022-05-13 11:57:20 -0500

    Initial Commit

---
+++ messagingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  messagingApp
+//
+//  Created by 90308616 on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ messagingApp/messagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  messagingAppApp.swift
+//  messagingApp
+//
+//  Created by 90308616 on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct messagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
